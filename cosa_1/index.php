5b8039aea12dd95fdc8e7b3bad9cd7542fe361f7
HectorPolanco1 <89503326+HectorPolanco1@users.noreply.github.com>
2023-05-04 23:13:16 -0600

    Avance 4/05/23
    Corrección de las colisiones y preparando el terreno para poder agregar al personaje al mini mapa, así como para los niveles (diseños en físico, todavía no se han pasado a digital).

--- cosa_1/index.php
+++ cosa_1/index.php
@@ -1,22 +1,25 @@
 <!DOCTYPE html>
+
 <html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="description" content="Juego 1, sin ánimos de pretender...">
-    <meta name="author" content = "Awful Games">
-    <title>Awful Games 1</title>
-</head>
-<body>
-<div id="juego">	
-	<div id="mapa">
-    </div>
-	<div id="jugador">
-	</div>
-    <div id="colisiones">
-	</div>	    				
-</div>
-<?php
-	include_once 'app/cargadorArchivosJS.inc.php';
-?>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="description" content="Juego 1, sin ánimos de pretender...">
+        <meta name="author" content = "Awful Games">
+        <title>Awful Games 1</title>
+    </head>
+    <body>
+        <div id="juego">	
+            <div id="mapa">
+            </div>
+            <div id="jugador">
+            </div>
+            <div id="colisiones">
+            </div>
+            <div id="localizaciones">
+            </div>	    				
+        </div>
+        <?php
+            include_once 'app/cargadorArchivosJS.inc.php';
+        ?>
+    </body>
 </html>
